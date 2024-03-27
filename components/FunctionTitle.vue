a8dcc617f5ddbfcfed72bfdc3028c67778c252fb
Jason Lee <huacnlee@gmail.com>
2024-03-27 16:43:05 +0800

    Improve stdlib doc generate to use function sign as the heading.

---
+++ components/FunctionTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="fn-title">
+        <slot></slot>
+        <a :href="`#${anchor}`" class="header-anchor" :id="anchor"></a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["anchor", "level", "title"],
+};
+</script>
